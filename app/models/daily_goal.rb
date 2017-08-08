283177d0f60eb2d4606da93df7d37351188cacee
momochi <momochi000@gmail.com>
2017-08-08 16:25:51 -0700

    Show indicator of daily goal progress on dashboard

--- app/models/daily_goal.rb
+++ app/models/daily_goal.rb
@@ -1,3 +1,31 @@
 class DailyGoal < ApplicationRecord
   belongs_to :user
+
+  delegate :todays_meals, :to => :user
+
+  def carbs_remaining
+    carbs - todays_carbs
+  end
+
+  def fat_remaining
+    fat - todays_fat
+  end
+
+  def protein_remaining
+    protein - todays_protein
+  end
+
+  private
+
+  def todays_carbs
+    todays_meals.map(&:carbs).compact.sum
+  end
+
+  def todays_fat
+    todays_meals.map(&:fat).compact.sum
+  end
+
+  def todays_protein
+    todays_meals.map(&:protein).compact.sum
+  end
 end
